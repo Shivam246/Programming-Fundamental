68b60d856f89a886a001adc4ad7a8ec9e3875946
Shivam246 <shivam.paudel07@gmail.com>
2016-12-04 20:53:03 -0800

    Add files via upload
    A C program to use relational operator

---
+++ relational.c
@@ -0,0 +1,22 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 10
+Program: C program to use relational operator
+Date: 05/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter any number");
+    scanf("%d",&a);
+    if(a>60 && a<80)
+    {
+        printf("You have passed your SLC with First Division");
+
+    }
+    getch();
+    return 0;
+}
