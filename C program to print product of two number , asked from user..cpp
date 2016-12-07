0f3d137ba17b98f9e4b33e8f55207b93ac83292f
Shivam246 <shivam.paudel07@gmail.com>
2016-12-07 08:45:44 -0800

    Add files via upload
    C program to print product of two number , asked from user.

---
+++ C program to print product of two number , asked from user..cpp
@@ -0,0 +1,20 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 10
+Program: C program to print product of two number , asked from user.
+Date: 07/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,product;
+	printf("Enter the first number");
+	scanf ("%d",&a);
+	printf("Enter the second number");
+	scanf("%d",&b);
+	product=a*b;
+	printf("The product of two number is %d",product);
+	getch();
+}
