285a48e1213b7af6a8fbb81f2b84295855b8066a
Shivam246 <shivam.paudel07@gmail.com>
2016-12-07 07:28:31 -0800

    Add files via upload
    C program to print a name , ask from user

---
+++ Naame print asking from user.cpp
@@ -0,0 +1,20 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 10
+Program: C program to use relational operator
+Date: 07/12/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char name [10];
+	printf("Enter your name");
+	scanf("%s",&name);
+	printf("Name of user is %s",name);
+	getch();
+	
+}
+
