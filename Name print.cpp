53d20e552c24b12b45f037df44c062f0e08e4f80
Shivam246 <shivam.paudel07@gmail.com>
2016-12-07 06:59:50 -0800

    Add files via upload
    C program to print the name..

---
+++ Name print.cpp
@@ -0,0 +1,15 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 10
+Program: C program to use relational operator
+Date: 07/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Shivam Paudel");
+	getch();
+}
+
