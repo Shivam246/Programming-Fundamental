361069a1f2595ece773d7d388314ff1bfed9643c
Shivam246 <shivam.paudel07@gmail.com>
2016-12-07 08:14:46 -0800

    Add files via upload
    C program to print sum of two numbers, asked from user.

---
+++ Sum of two number asked from user.cpp
@@ -0,0 +1,20 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 10
+Program: C program to print sum of two number , asked from user.
+Date: 07/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter the first number");
+	scanf ("%d",&a);
+	printf("Enter the second number");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("The sum of two product is %d",sum);
+	getch();
+}
