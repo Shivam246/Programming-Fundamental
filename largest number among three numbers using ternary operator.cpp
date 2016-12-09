16f53eb6f70307d12b73d3792e4d83dfb404fac6
Shivam246 <shivam.paudel07@gmail.com>
2016-12-08 21:16:10 -0800

    Add files via upload
    C program to find largest number among three numbers using ternary operator

---
+++ largest number among three numbers using ternary operator.cpp
@@ -0,0 +1,23 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 12
+Program: C program to print the largest number among three number using ternary operator.
+Date: 09/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+		printf("Enter first number\n");
+		scanf("%d",&a);
+		printf("Enter second number\n");
+		scanf("%d",&b);
+		printf("Enter third number\n");
+		scanf("%d",&c);
+		(a>=b&&a>=c)?printf("The largest number is %d",a):(b>=a&&b>=a)?printf("The largest number is %d",b):printf("The largest number is %d",c);
+		getch();
+		return(0);
+		
+}
