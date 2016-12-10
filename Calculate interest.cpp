56d4371096b84ba7390a2d15cd5433c08f2d809f
Shivam246 <shivam.paudel07@gmail.com>
2016-12-10 05:13:08 -0800

    Add files via upload

---
+++ Calculate interest.cpp
@@ -0,0 +1,125 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 2
+Program: C program to print simple interest, principal,time and rate asked from user.
+Date: 07/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float P,T,R,I;
+	printf("Enter principal");
+	scanf("%f",&P);
+	printf("Enter rate ");
+	scanf("%f",&R);
+	printf("Enter time ");
+	scanf("%f",&T);
+	I=P*T*R/100;
+	printf("The required interest is %f",I);
+	getch();
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
