6a903713ad918e0a635217f293e0950005776f7e
Shivam246 <shivam.paudel07@gmail.com>
2016-12-09 19:52:57 -0800

    Add files via upload
    C program to print the division between two number, asked from user..

---
+++ Division asked from user.cpp
@@ -0,0 +1,20 @@
+/*
+Student Name: Shivam Paudel
+Roll no:
+Lab Sheet no: 10
+Program: C program to find the division between two number , asked from user.
+Date: 10/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float a,b,division;
+	printf("Enter the first number");
+	scanf ("%f",&a);
+	printf("Enter the second number");
+	scanf("%f",&b);
+	division=a/b;
+	printf("The quotient of two number is %f",division);
+	getch();
+}
